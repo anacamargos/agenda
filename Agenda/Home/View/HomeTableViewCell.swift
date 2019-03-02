0ed1478c2fe9a4ff382fae2093921a96984b754c
Ana <alcvlara@sga.pucminas.br>
2019-03-02 16:29:10 -0300

    Creating .gitignore

---
+++ Agenda/Home/View/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  Agenda
+//
+//  Created by Ana Leticia Camargos on 02/03/19.
+//  Copyright © 2019 Ana Leticia Camargos. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
