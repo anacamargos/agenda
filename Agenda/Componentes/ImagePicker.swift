b01ec95009509d3448e67bdd952aef55ddc97738
Ana <alcvlara@sga.pucminas.br>
2019-03-04 12:08:28 -0300

    Adding access to photo library

--- Agenda/Componentes/ImagePicker.swift
+++ Agenda/Componentes/ImagePicker.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum MenuOpcoes {
+    case camera
+    case biblioteca
+}
+
 protocol ImagePickerFotoSelecionada {
     func imagePickerFotoSelecionada(_ foto: UIImage)
 }
@@ -23,5 +28,28 @@ class ImagePicker: NSObject, UIImagePickerControllerDelegate, UINavigationContro
         let foto = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
         delegate?.imagePickerFotoSelecionada(foto)
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func menuDeOpcoes(completion: @escaping(_ opcao: MenuOpcoes) -> Void ) -> UIAlertController {
+        let menu = UIAlertController(title: "Atenção", message: "Escolha uma da opções abaixo", preferredStyle: .actionSheet)
+        
+        let camera = UIAlertAction(title: "Tirar foto", style: .default) { (acao) in
+            // é o que vai fazer quando o usuário clicar na opcao camera
+            completion(.camera)
+        }
+        menu.addAction(camera)
+        
+        let biblioteca = UIAlertAction(title: "Biblioteca", style: .default) { (acao) in
+            // é o que vai ser executado quando o usuário clicar nessa opcao
+            completion(.biblioteca)
+        }
+        menu.addAction(biblioteca)
+        
+        let cancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        menu.addAction(cancelar)
+        
+        return menu
+        
     }
 }
